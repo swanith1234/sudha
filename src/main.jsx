5492763539720f497d7c7e632d424f821ddf62dd
swanith1234 <swanithpidugu@gmail.com>
2024-12-07 06:47:10 +0530

    Add folder: src

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App userData={{"name":"sudha","emailId":"pidugubunny534@gmail.com","phoneNo":"2763542364","projects":[{"title":"sudha","description":"sudha","technologies":["React"],"repoLink":"sudha","_id":"6753a19cae8292836be3b97c"},{"title":"sudha","description":"sudha","technologies":["React","C++"],"repoLink":"sudha","_id":"6753a19cae8292836be3b97d"}],"resume":"","experiences":[{"companyName":"sudha","logo":"http://res.cloudinary.com/dgthxi1sd/image/upload/v1733534067/evolumin/saelvohuvfpcqnwpewnt.png","role":"sudha","description":"sudha","technologiesUsed":[],"duration":"sudha","_id":"6753a19cae8292836be3b97e"},{"companyName":"sudha","logo":"http://res.cloudinary.com/dgthxi1sd/image/upload/v1733534086/evolumin/xbakvi7etwsodm17klgj.jpg","role":"sudha","description":"sudha","technologiesUsed":[],"duration":"sudha","_id":"6753a19cae8292836be3b97f"}],"techStacks":["React","C++"],"contactDetails":["sudha"],"codingProfiles":["sudha"],"certifications":["https://res.cloudinary.com/dgthxi1sd/image/upload/v1733531771/evolumin/ngmze4qcgooxihekx5lf.jpg","https://res.cloudinary.com/dgthxi1sd/image/upload/v1733531771/evolumin/ngmze4qcgooxihekx5lf.jpg"],"achievements":[{"title":"https://res.cloudinary.com/dgthxi1sd/image/upload/v1733531771/evolumin/ngmze4qcgooxihekx5lf.jpg","description":"dd","_id":"6753a19cae8292836be3b980"}],"about":"dssd","profilePhoto":"http://res.cloudinary.com/dgthxi1sd/image/upload/v1733534048/evolumin/obpiikojw3spqziiuzvp.jpg","_id":"6753a19cae8292836be3b97b","createdAt":"2024-12-07T01:15:08.879Z","updatedAt":"2024-12-07T01:15:08.879Z","__v":0}} />
+  </StrictMode>,
+)
